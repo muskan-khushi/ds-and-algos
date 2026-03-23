6a9f491a158e8c026b22cbb5e83bb55ac3017065
Muskan <muskankhushi2022@gmail.com>
2026-03-23 13:13:31 +0530

    add solution for Helpful Maths problem

---
+++ Competitive_Programming/helpfulMaths.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s;
+    cin >> s;
+
+    vector<int> numbers;
+    for (int i=0; i<s.size(); i+=2){
+        numbers.push_back(s[i] - '0');
+    }    
+    sort(numbers.begin(), numbers.end());
+    for (int i=0; i<numbers.size(); i++){
+        cout << numbers[i];
+        if (i != numbers.size()-1) cout << "+";
+    }
+
+
+    return 0;
+}
