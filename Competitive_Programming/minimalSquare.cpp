667c1085d34fff8767c9abd0593096c92259ef53
Muskan <muskankhushi2022@gmail.com>
2026-03-23 18:43:51 +0530

    refactor minimalSquare.cpp by commenting out an alternative implementation

--- Competitive_Programming/minimalSquare.cpp
+++ Competitive_Programming/minimalSquare.cpp
@@ -25,3 +25,22 @@ int main(){
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+//     int t;
+//     cin >> t;
+
+//     while (t--){
+//         int a, b;
+//         cin >> a >> b;
+
+//         int side = max(max(a, b), 2 * min(a, b));
+//         cout << side * side << endl;
+//     }
+
+//     return 0;
+// }
