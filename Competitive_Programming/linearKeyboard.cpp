20748db389bf9c56220cdc2a49bc5c81a29209ed
Muskan <muskankhushi2022@gmail.com>
2026-03-24 09:47:29 +0530

    refactor linearKeyboard.cpp to improve character position mapping

--- Competitive_Programming/linearKeyboard.cpp
+++ Competitive_Programming/linearKeyboard.cpp
@@ -12,8 +12,17 @@ int main(){
         string s;
         cin >> s;
 
-        int time = 0;
+        vector<int> pos(26);
+        for (int i=0; i<26; i++){
+            pos[keyboard[i] - 'a'] = i;
+        } 
         
+        int time = 0;
+        for (int i=1; i<s.size(); i++){
+            time += abs(pos[s[i] - 'a'] - pos[s[i-1] - 'a']);
+        }
+
+        cout << time << endl;
     }
 
     return 0;
