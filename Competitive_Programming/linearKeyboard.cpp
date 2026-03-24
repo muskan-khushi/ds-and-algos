c1ecef1b7f1b29c9dfb60ec46b3bf872d3336595
Muskan <muskankhushi2022@gmail.com>
2026-03-24 09:11:49 +0530

    add initial implementation for linear keyboard problem and wrong subtraction problem

---
+++ Competitive_Programming/linearKeyboard.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+    while (t--){
+        string keyboard;
+        cin >> keyboard;
+
+        string s;
+        cin >> s;
+
+        int time = 0;
+        
+    }
+
+    return 0;
+}
