1eb10a7c3ae0f12b016e22597b445bf8853b3a55
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-24 19:20:57 +0530

    ✅ Set Matrix Zeroes | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 20.8 MB | 2026-08-24

--- leetcode/medium/0073-set-matrix-zeroes/solution.cpp
+++ leetcode/medium/0073-set-matrix-zeroes/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size(); //rows
+        int m = matrix.size();
         int n = matrix[0].size();
-        
+
         bool firstRowZero = false;
         for (int i=0; i<n; i++){
             if (matrix[0][i] == 0){
@@ -13,8 +13,8 @@ public:
         }
 
         bool firstColZero = false;
-        for (int i=0; i<m; i++){
-            if (matrix[i][0] == 0){
+        for (int j=0; j<m; j++){
+            if (matrix[j][0] == 0){
                 firstColZero = true;
                 break;
             }
@@ -29,23 +29,23 @@ public:
             }
         }
 
-        for (int i=1; i<m; i++){
+        for (int  i=1; i<m; i++){
             for (int j=1; j<n; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                if (matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
 
         if (firstRowZero){
-            for (int j=0; j<n; j++){
-                matrix[0][j] = 0;
+            for (int i=0; i<n; i++){
+                matrix[0][i] = 0;
             }
         }
 
         if (firstColZero){
-            for (int j=0; j<m; j++){
-                matrix[j][0] = 0;
+            for (int i=0; i<m; i++){
+                matrix[i][0] = 0;
             }
         }
     }
