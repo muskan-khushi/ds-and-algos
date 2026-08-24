a5e23833146038f559cd5ed23d9e69b09be64232
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-24 13:18:46 +0530

    ✅ Spiral Matrix | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 9.4 MB | 2026-08-24

---
+++ leetcode/medium/0054-spiral-matrix/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> ans;
+
+        int top = 0;
+        int bottom = n-1;
+        int left = 0;
+        int right = m-1;
+
+        while (left  <= right && top <= bottom){
+            //print top
+            for (int i=left; i<=right; i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            //print right
+            for (int i=top; i<=bottom; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            //print bottom
+            if (top <= bottom){
+            for (int i=right; i>=left; i--){
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            }
+
+            //print left
+            if (left <= right){
+            for (int i=bottom; i>=top; i--){
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+            }
+        }
+
+        return ans;
+    }
+};
