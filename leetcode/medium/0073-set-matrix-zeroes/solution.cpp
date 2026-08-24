c346f280dfd28ab9bbed90ff011e1db1a2514a79
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-24 19:12:57 +0530

    ✅ Set Matrix Zeroes | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 21.1 MB | 2026-08-24

--- leetcode/medium/0073-set-matrix-zeroes/solution.cpp
+++ leetcode/medium/0073-set-matrix-zeroes/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
+        int m = matrix.size(); //rows
         int n = matrix[0].size();
-
+        
         bool firstRowZero = false;
         for (int i=0; i<n; i++){
             if (matrix[0][i] == 0){
@@ -31,7 +31,7 @@ public:
 
         for (int i=1; i<m; i++){
             for (int j=1; j<n; j++){
-                if (matrix[i][0] == 0 || matrix[0][j] == 0){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
