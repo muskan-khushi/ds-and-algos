0273d60cc2a2c5a0d7de38fbf567c70249854534
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-24 12:10:16 +0530

    ✅ reverse-words-in-a-given-string5459 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ gfg/medium/reverse-words-in-a-given-string5459/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    string reverseWords(string &s) {
+        // code here
+        int n = s.size();
+        vector<string> words;
+        string word = "";
+        
+        for (char ch : s){
+            if (ch == '.'){
+                if (!word.empty()){
+                    words.push_back(word);
+                    word = "";
+                }
+            }
+            else word += ch;
+        }
+        
+        if (!word.empty()) words.push_back(word);
+        
+        string ans = "";
+        for (int i=words.size()-1; i>=0; i--){
+            ans += words[i];
+            if (i != 0) ans.push_back('.');
+        }
+        
+        return ans;
+    }
+};
