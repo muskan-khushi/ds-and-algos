6cd8245f3821356442a9c88ffb9217aa5ada9b04
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-24 13:25:47 +0530

    ✅ Spiral Matrix | Medium | C++ | Runtime: 2 ms (1.81%) | Memory: 9.4 MB | 2026-08-24

--- leetcode/medium/0054-spiral-matrix/solution.cpp
+++ leetcode/medium/0054-spiral-matrix/solution.cpp
@@ -7,38 +7,41 @@ public:
         vector<int> ans;
 
         int top = 0;
-        int bottom = n-1;
-        int left = 0;
-        int right = m-1;
+        int bottom = n - 1;
 
-        while (left  <= right && top <= bottom){
-            //print top
-            for (int i=left; i<=right; i++){
+        int left = 0;
+        int right = m - 1;
+
+        while (top <= bottom && left <= right) {
+            // print top
+            for (int i = left; i <= right; i++) {
                 ans.push_back(matrix[top][i]);
             }
             top++;
 
-            //print right
-            for (int i=top; i<=bottom; i++){
+            // print right
+            for (int i = top; i <= bottom; i++) {
                 ans.push_back(matrix[i][right]);
             }
             right--;
 
-            //print bottom
-            if (top <= bottom){
-            for (int i=right; i>=left; i--){
-                ans.push_back(matrix[bottom][i]);
+            // print bottom
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(matrix[bottom][i]);
+                }
             }
             bottom--;
+            
+            //print left
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
             }
 
-            //print left
-            if (left <= right){
-            for (int i=bottom; i>=top; i--){
-                ans.push_back(matrix[i][left]);
-            }
-            left++;
-            }
+            
         }
 
         return ans;
