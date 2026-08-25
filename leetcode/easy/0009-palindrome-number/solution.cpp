c17e6dc18351d98499bfab3186f2f732b89604ca
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-25 15:44:41 +0530

    ✅ Palindrome Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 10.9 MB | 2026-08-25

--- leetcode/easy/0009-palindrome-number/solution.cpp
+++ leetcode/easy/0009-palindrome-number/solution.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
+
         string s = to_string(x);
+
         int n = s.size();
+        
         int i = 0;
         int j = n-1;
+
         while (i<j){
             if (s[i] != s[j]) return false;
             i++;
