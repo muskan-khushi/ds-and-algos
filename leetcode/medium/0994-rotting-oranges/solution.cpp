b39d99e125c490b6572dbba9c6169e6712e57a7c
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-25 08:54:34 +0530

    ✅ Rotting Oranges | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 16.7 MB | 2026-08-25

---
+++ leetcode/medium/0994-rotting-oranges/solution.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<pair<int, int>> q;
+
+        int fresh = 0;
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (grid[i][j] == 2){
+                    q.push({i,j});
+                }
+                else if (grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+
+        int minutes = 0;
+
+        while (!q.empty() && fresh > 0){
+            int size = q.size();
+
+            while (size--){
+                auto [r,c] = q.front();
+                q.pop();
+
+                int dr[] = {-1,0,1,0};
+                int dc[] = {0,-1,0,1};
+
+                for (int k=0; k<4; k++){
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
+
+                    if (nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1){
+                        grid[nr][nc] = 2;
+                        q.push({nr,nc});
+                        fresh--;
+                    }
+                }
+            }
+
+            minutes++;
+
+        }
+        if (fresh > 0) return -1;
+
+        return minutes;
+        
+    }
+};
