179bcb9feba66c84d8dd84e7af0d1cf06ded6221
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-26 08:57:02 +0530

    ✅ Rotting Oranges | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 16.6 MB | 2026-08-26

--- leetcode/medium/0994-rotting-oranges/solution.cpp
+++ leetcode/medium/0994-rotting-oranges/solution.cpp
@@ -3,11 +3,14 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
+        
+        //to store rotten oranges
         queue<pair<int, int>> q;
 
+        //to store fresh count
         int fresh = 0;
 
+        //storing rotten oranges in queue and tracking fresh count
         for (int i=0; i<m; i++){
             for (int j=0; j<n; j++){
                 if (grid[i][j] == 2){
@@ -19,17 +22,20 @@ public:
             }
         }
 
+        //initializing minutes
         int minutes = 0;
 
+        //all turns
         while (!q.empty() && fresh > 0){
             int size = q.size();
 
+            //one turn -> one level
             while (size--){
                 auto [r,c] = q.front();
                 q.pop();
 
-                int dr[] = {0,-1,0,1};
-                int dc[] = {1,0,-1,0};
+                int dr[] = {-1,0,1,0};
+                int dc[] = {0,-1,0,1};
 
                 for (int k=0; k<4; k++){
                     int nr = r + dr[k];
@@ -44,8 +50,8 @@ public:
             }
             minutes++;
         }
-
         if (fresh > 0) return -1;
         return minutes;
+        
     }
 };
