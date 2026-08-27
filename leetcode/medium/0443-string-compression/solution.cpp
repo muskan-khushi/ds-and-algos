ca8ef6a0145a58a895afe9796ae20948c0d612ea
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-27 11:03:44 +0530

    ✅ String Compression | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 13.9 MB | 2026-08-27

--- leetcode/medium/0443-string-compression/solution.cpp
+++ leetcode/medium/0443-string-compression/solution.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
+        //which position to write
         int write = 0;
+
+        //scan the chars
         int i = 0;
 
-        while (i < chars.size()){
-            char currChar = chars[i];
+        while (i<chars.size()){
+            char currentChar = chars[i];
             int j = i;
-            while (j< chars.size() && chars[j] == currChar ){
+
+            while (j<chars.size() && chars[j] == currentChar){
                 j++;
             }
-            int count = j - i;
 
-            chars[write] = currChar;
+            int count = j-i;
+
+            chars[write] = currentChar;
             write++;
 
             if (count > 1){
                 string countStr = to_string(count);
-                for (char digit : countStr){
-                    chars[write] = digit;
+                for (char ch : countStr){
+                    chars[write] = ch;
                     write++;
                 }
             }
 
-             i = j;
-        }   
+            i=j;
+         }
 
-        return write;     
+         return write;
     }
 };
