27b725a77d8139e1d19422599c46e0755eac3d52
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-08-30 09:38:55 +0530

    ✅ Perfect Number | Easy | C++ | Runtime: 2080 ms (28.13%) | Memory: 7.9 MB | 2026-08-30

---
+++ leetcode/easy/0507-perfect-number/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int ans = 0;
+        for (int i=1; i<num; i++){
+            if (num % i == 0) ans += i;
+        }
+        return ans == num;
+    }
+};
