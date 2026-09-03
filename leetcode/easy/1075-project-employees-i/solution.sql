54967616b4c9f24bde75b59c4acd62469e00ec7b
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-03 08:52:04 +0530

    ✅ Project Employees I | Easy | SQL | Runtime: 152 ms | Memory: 0B | 2026-09-03

---
+++ leetcode/easy/1075-project-employees-i/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT Project.project_id, AVG(Employee.experience_years) AS average_years
+FROM Project
+JOIN Employee
+ON Project.employee_id = Employee.employee_id
+GROUP BY Project.project_id;
