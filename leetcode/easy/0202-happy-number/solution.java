bec0d2b247aa88e52cf8a62b6a20bf57228ceb5b
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-08 17:03:46 +0530

    ✅ Happy Number | Easy | Java | Runtime: 1 ms (77.61%) | Memory: 42.2 MB | 2026-09-08

--- leetcode/easy/0202-happy-number/solution.java
+++ leetcode/easy/0202-happy-number/solution.java
@@ -1,7 +1,7 @@
 class Solution {
     private int getNext(int n){
         int sum = 0;
-        while (n > 0){
+        while (n>0){
             int digit = n%10;
             sum += digit*digit;
             n /= 10;
@@ -11,6 +11,7 @@ class Solution {
     public boolean isHappy(int n) {
         int slow = n;
         int fast = n;
+
         do {
             slow = getNext(slow);
             fast = getNext(getNext(fast));
