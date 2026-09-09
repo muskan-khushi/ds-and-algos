c2954bff3bbb8dcf25fcf2c18c5bbdc4a1de1e2f
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-09 11:06:41 +0530

    ✅ Cycle Length in Linked List | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ gfg/medium/find-length-of-loop/solution.java
@@ -0,0 +1,42 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public int lengthOfLoop(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+        
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) break;
+        }
+        
+        if (fast == null || fast.next == null) return 0;
+        
+        Node entry = head;
+        while (entry != slow){
+            slow = slow.next;
+            entry = entry.next;
+        }
+        
+        int length = 1;
+        
+        Node curr = entry.next;
+        while (curr != entry){
+            curr = curr.next;
+            length++;
+        }
+        return length;
+    }
+}
