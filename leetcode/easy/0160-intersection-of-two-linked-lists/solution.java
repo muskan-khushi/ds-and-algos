1f5e385c3a24673001237adc5f6e0c10f437238d
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-09 09:55:10 +0530

    ✅ Intersection of Two Linked Lists | Easy | Java | Runtime: 1 ms (99.92%) | Memory: 53 MB | 2026-09-09

--- leetcode/easy/0160-intersection-of-two-linked-lists/solution.java
+++ leetcode/easy/0160-intersection-of-two-linked-lists/solution.java
@@ -15,10 +15,9 @@ public class Solution {
         ListNode t2 = headB;
 
         while (t1 != t2){
-           t1 = t1==null ? headB : t1.next;
-           t2 = t2==null ? headA : t2.next;
+            t1 = t1==null?headB:t1.next;
+            t2 = t2==null?headA:t2.next;
         }
-
         return t1;
     }
 }
