f503e7ec178ffe9929c1db2919232d5417595d74
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-11 15:05:36 +0530

    ✅ Koko Eating Bananas | Medium | Java | Runtime: 7 ms (83.10%) | Memory: 48 MB | 2026-09-11

--- leetcode/medium/0875-koko-eating-bananas/solution.java
+++ leetcode/medium/0875-koko-eating-bananas/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
         int left = 1;
         int right = 0;
         for (int pile : piles){
-            right = Math.max(pile, right);
+            right = Math.max(right, pile);
         }
+
         while (left < right){
             int mid = left + (right-left)/2;
             int hours = 0;
+            
+            for (int pile : piles) hours += (mid+pile-1)/mid;
 
-            for (int pile : piles){
-                hours += (pile+mid-1)/mid;
-            }
             if (hours <= h) right = mid;
             else left = mid+1;
         }
