dc4d593e277eafb190cd4d655eac5af5f9304041
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-12 10:25:50 +0530

    ✅ Minimum Absolute Sum Difference | Medium | Java | Runtime: 73 ms (29.21%) | Memory: 89.1 MB | 2026-09-12

--- leetcode/medium/1818-minimum-absolute-sum-difference/solution.java
+++ leetcode/medium/1818-minimum-absolute-sum-difference/solution.java
@@ -8,28 +8,29 @@ class Solution {
         int maxGain = 0;
 
         for (int i=0; i<n; i++){
-            int oldDiff = Math.abs(nums1[i] - nums2[i]);
-            total += oldDiff;
+        int oldDiff = Math.abs(nums2[i] - nums1[i]);
+        total += oldDiff;
 
-            int index = lowerBound(sorted, nums2[i]);
-            if (index < n){
-                int newDiff = Math.abs(sorted[index] - nums2[i]);
-                int gain = oldDiff - newDiff;
-                maxGain = Math.max(gain, maxGain);
-            }
-            if (index > 0){
-                int newDiff = Math.abs(sorted[index-1]-nums2[i]);
-                int gain = oldDiff - newDiff;
-                maxGain = Math.max(gain, maxGain);
-            }
+        int index = binarySearch(sorted, nums2[i]);
+        if (index < n){
+            int newDiff = Math.abs(sorted[index] - nums2[i]);
+            int gain = oldDiff - newDiff;
+            maxGain = Math.max(gain, maxGain);
         }
-        return (int)((total-maxGain)%1_000_000_007);
+        if (index > 0){
+            int newDiff = Math.abs(sorted[index-1] - nums2[i]);
+            int gain = oldDiff - newDiff;
+            maxGain = Math.max(gain, maxGain);
+        }
+        }
+        return (int)((total-maxGain)%1000000007);
+
     }
 
-    private int lowerBound(int[] arr, int target){
+    private int binarySearch(int[] arr, int target){
         int left = 0;
         int right = arr.length;
-
+        
         while (left < right){
             int mid = left + (right-left)/2;
             if (arr[mid] >= target) right = mid;
