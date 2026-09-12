2241cf09ed95eb450558f97bb9223f026098e317
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-12 21:09:00 +0530

    ✅ Minimum Number of Days to Make m Bouquets | Medium | Java | Runtime: 16 ms (91.38%) | Memory: 82.9 MB | 2026-09-12

--- leetcode/medium/1482-minimum-number-of-days-to-make-m-bouquets/solution.java
+++ leetcode/medium/1482-minimum-number-of-days-to-make-m-bouquets/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length;
+
         if ((long)m*k > n) return -1;
 
         int left = Integer.MAX_VALUE;
         int right = Integer.MIN_VALUE;
-
+        
         for (int bloom : bloomDay){
             left = Math.min(left, bloom);
             right = Math.max(right, bloom);
         }
 
         while (left < right){
-            int mid = left + (right - left)/2;
+            int mid = left + (right-left)/2;
 
             int consecutive = 0;
             int bouquets = 0;
@@ -20,18 +21,21 @@ class Solution {
             for (int day : bloomDay){
                 if (day <= mid){
                     consecutive++;
+
                     if (consecutive == k){
                         bouquets++;
                         consecutive = 0;
                     }
                 }
-                else consecutive = 0;
+                else {
+                    consecutive = 0;
+                }
             }
 
             if (bouquets >= m) right = mid;
             else left = mid+1;
-        }
 
+        }
         return left;
     }
 }
