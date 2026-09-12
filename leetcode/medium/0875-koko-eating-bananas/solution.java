2c4e60ba3c1210236961fd870d4c96c26ae88e87
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-12 09:22:25 +0530

    ✅ Koko Eating Bananas | Medium | Java | Runtime: 7 ms (83.23%) | Memory: 47.7 MB | 2026-09-12

--- leetcode/medium/0875-koko-eating-bananas/solution.java
+++ leetcode/medium/0875-koko-eating-bananas/solution.java
@@ -6,13 +6,12 @@ class Solution {
         for (int pile : piles){
             right = Math.max(right, pile);
         }
-
         while (left < right){
             int mid = left + (right-left)/2;
             int hours = 0;
-            
-            for (int pile : piles) hours += (mid+pile-1)/mid;
-
+            for (int pile : piles){
+                hours += (mid + pile - 1)/mid;
+            }
             if (hours <= h) right = mid;
             else left = mid+1;
         }
