ae783485f060d5c25424755db8683ec2a922cd19
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-13 09:18:20 +0530

    ✅ Kth Smallest Element in a Sorted Matrix | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 52.2 MB | 2026-09-13

--- leetcode/medium/0378-kth-smallest-element-in-a-sorted-matrix/solution.java
+++ leetcode/medium/0378-kth-smallest-element-in-a-sorted-matrix/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
+
         int left = matrix[0][0];
         int right = matrix[n-1][n-1];
 
         while (left < right){
             int mid = left + (right-left)/2;
-            int count = countLessEqual(matrix, mid);
 
-            if (count < k) left = mid+1;
+            int ans = countLessEqual(matrix, mid);
+
+            if (ans < k) left = mid+1;
             else right = mid;
         }
         return left;
@@ -22,9 +24,9 @@ class Solution {
 
         int count = 0;
 
-        while (row >= 0 && col < n){
+        while (row>=0 && col < n){
             if (matrix[row][col] <= target){
-                count += row + 1;
+                count += row+1;
                 col++;
             }
             else row--;
