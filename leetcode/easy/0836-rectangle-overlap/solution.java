19e8c59f882c3fdbf300c272e9f60a2ee4186e69
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-14 08:10:18 +0530

    ✅ Rectangle Overlap | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.8 MB | 2026-09-14

--- leetcode/easy/0836-rectangle-overlap/solution.java
+++ leetcode/easy/0836-rectangle-overlap/solution.java
@@ -6,9 +6,7 @@ class Solution {
         //rec1[0], rec1[3] - bottom left
         //rec1[2],rec1[3] - bottom right
 
-        boolean xOverlap = Math.max(rec1[0],rec2[0]) < Math.min(rec1[2],rec2[2]);
-        boolean yOverlap = Math.max(rec1[1],rec2[1]) < Math.min(rec2[3],rec1[3]);
-
-        return xOverlap && yOverlap;
+        if (rec1[2] <= rec2[0] || rec2[2] <= rec1[0] || rec1[3] <= rec2[1] || rec2[3] < rec1[1]) return false;
+        return true; 
     }
 }
