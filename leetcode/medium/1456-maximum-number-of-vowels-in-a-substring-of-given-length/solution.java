a9ea27d95b575ca2651822e8eeb06dbcb6afe770
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-17 09:41:50 +0530

    ✅ Maximum Number of Vowels in a Substring of Given Length | Medium | Java | Runtime: 14 ms (50.26%) | Memory: 46.3 MB | 2026-09-17

--- leetcode/medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/solution.java
+++ leetcode/medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/solution.java
@@ -1,25 +1,22 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int n = s.length();
         int left = 0;
         int vowelCount = 0;
         int maxCount = 0;
-        for (int right = 0; right<n; right++){
-            if (s.charAt(right) == 'a' || s.charAt(right) == 'e' || s.charAt(right) == 'i' || s.charAt(right) == 'o' || s.charAt(right) == 'u') vowelCount++;
 
-            if (right-left+1 == k){
-                maxCount = Math.max(vowelCount, maxCount);
-                if (s.charAt(left) == 'a' ||
-                    s.charAt(left) == 'e' ||
-                    s.charAt(left) == 'i' ||
-                    s.charAt(left) == 'o' ||
-                    s.charAt(left) == 'u') {
-
-                    vowelCount--;
-                }
+        for (int right = 0; right < s.length(); right++){
+            if (isVowel(s.charAt(right))) vowelCount++;
+            if (right - left + 1 == k){
+                maxCount = Math.max(maxCount, vowelCount);
+                if (isVowel(s.charAt(left))) vowelCount--;
                 left++;
             }
         }
+
         return maxCount;
+    }
+
+    private boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c=='i' || c=='o' || c=='u';
     }
 }
