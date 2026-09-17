dd9c2327871b176380845fe852a11acdb97b8562
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-17 08:47:45 +0530

    ✅ Longest Repeating Character Replacement | Medium | Java | Runtime: 6 ms (98.92%) | Memory: 46.2 MB | 2026-09-17

---
+++ leetcode/medium/0424-longest-repeating-character-replacement/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0;
+        int maxFreq = 0;
+
+        int ans = 0;
+
+        for (int right = 0; right<s.length(); right++){
+            int index = s.charAt(right) - 'A';
+            freq[index]++;
+            maxFreq = Math.max(freq[index], maxFreq);
+
+            while ((right - left + 1) - maxFreq > k){
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            ans = Math.max(right-left+1, ans);
+        }
+        return ans;
+    }
+}
