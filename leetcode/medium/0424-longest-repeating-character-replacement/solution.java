8a0565faefc9ed51d9f44f24203aed617af68ea5
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-17 08:56:35 +0530

    ✅ Longest Repeating Character Replacement | Medium | Java | Runtime: 7 ms (89.24%) | Memory: 45.6 MB | 2026-09-17

--- leetcode/medium/0424-longest-repeating-character-replacement/solution.java
+++ leetcode/medium/0424-longest-repeating-character-replacement/solution.java
@@ -1,22 +1,33 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        //Algorithm
+        //Dynamic Sliding Window Pattern
+        // kisi bhi window mei jis bhi element ka maxFreq hogaa, window length - maxFreq <= k hona chhaiye
+
+        //store frequency of each element in a particular window
         int[] freq = new int[26];
-        int left = 0;
+
+        //max freq of any element in a window
         int maxFreq = 0;
 
+        //ans storing the max length
         int ans = 0;
+        int left = 0;
 
+        //sliding window pattern
         for (int right = 0; right<s.length(); right++){
             int index = s.charAt(right) - 'A';
             freq[index]++;
-            maxFreq = Math.max(freq[index], maxFreq);
+            maxFreq = Math.max(maxFreq, freq[index]);
 
-            while ((right - left + 1) - maxFreq > k){
+           //invalid window, shrink from left
+            while ((right-left+1) - maxFreq > k){
                 freq[s.charAt(left) - 'A']--;
                 left++;
             }
 
-            ans = Math.max(right-left+1, ans);
+            //valid window
+            ans = Math.max(ans, right-left+1);
         }
         return ans;
     }
