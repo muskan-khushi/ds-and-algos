ec9063e71ab397fb15d805b26e6ede9a2529ca95
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-18 11:38:24 +0530

    ✅ Maximum Erasure Value | Medium | Java | Runtime: 51 ms (69.99%) | Memory: 92.1 MB | 2026-09-18

--- leetcode/medium/1695-maximum-erasure-value/solution.java
+++ leetcode/medium/1695-maximum-erasure-value/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
+        //max sum of subarray of unique elements
         HashSet<Integer> set = new HashSet<>();
-        int left = 0;
         int sum = 0;
         int maxSum = 0;
+        int left = 0;
 
-        for (int right = 0; right <nums.length; right++){
+        for (int right = 0; right<nums.length; right++){
             while (set.contains(nums[right])){
                 set.remove(nums[left]);
                 sum -= nums[left];
@@ -14,7 +15,7 @@ class Solution {
             set.add(nums[right]);
             sum += nums[right];
 
-            maxSum = Math.max(sum, maxSum);
+            maxSum = Math.max(maxSum, sum);
         }
         return maxSum;
     }
