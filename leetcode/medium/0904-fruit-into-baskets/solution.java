6abe298611a2ca8d7c37af70db5ef219961331ff
Muskan <135226033+muskan-khushi@users.noreply.github.com>
2026-09-21 16:25:49 +0530

    ✅ Fruit Into Baskets | Medium | Java | Runtime: 53 ms (77.80%) | Memory: 70.4 MB | 2026-09-21

--- leetcode/medium/0904-fruit-into-baskets/solution.java
+++ leetcode/medium/0904-fruit-into-baskets/solution.java
@@ -1,21 +1,19 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         int n = fruits.length;
-        int left = 0;
         int max = 0;
+        int left = 0;
 
         HashMap<Integer, Integer> freq = new HashMap<>();
 
-        for (int right = 0; right < n; right++){
-            freq.put(fruits[right],freq.getOrDefault(fruits[right],0)+1);
+        for (int right=0; right<n; right++){
+            freq.put(fruits[right], freq.getOrDefault(fruits[right],0)+1);
             while (freq.size() > 2){
                 freq.put(fruits[left], freq.get(fruits[left])-1);
                 if (freq.get(fruits[left]) == 0) freq.remove(fruits[left]);
                 left++;
             }
-
-            int len = right - left + 1;
-            max = Math.max(len, max);
+            max = Math.max(max, right-left+1);
         }
         return max;
     }
